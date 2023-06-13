37082fe2a06c46b4ed9113b0deb8a1e682a005db
SergioCamejo <sergiofcamejo@yahoo.com.ar>
2023-06-12 21:05:48 -0300

    Carrito funcionando y con estilos

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -1,58 +1,109 @@
 <template>
+  <div>
+    <ul class="list-group">
+      <li
+        v-for="producto in productos"
+        :key="producto.nombre"
+        class="list-group-item"
+      >
+        {{ producto.nombre }} - Precio: ${{ producto.precio }}
+        <button
+          @click="agregarProducto(producto)"
+          class="btn btn-primary btn-sm"
+        >
+          Agregar
+        </button>
+      </li>
+    </ul>
     <div>
-      <div v-if="productos.length === 0">
-        <p>No hay productos en el carrito.</p>
-      </div>
-      <div v-else>
-        <ul>
-          <li v-for="producto in productos" :key="producto.nombre">
-            {{ producto.nombre }} - Precio: {{ producto.precio }}
-            <button @click="agregarProducto(producto)">Agregar</button>
-          </li>
-        </ul>
-        <p>Total: {{ calcularTotal() }}</p>
-        <button @click="vaciarCarrito">Vaciar carrito</button>
-        <button @click="realizarCompra" :disabled="carrito.length === 0">Realizar compra</button>
-      </div>
+      <p v-if="carrito.length > 0" class="mt-3">
+        Total: ${{ calcularTotal() }}
+      </p>
+      <p v-if="carrito.length > 0">Productos agregados:</p>
+      <ul v-if="carrito.length > 0">
+        <li v-for="producto in carrito" :key="producto.nombre">
+          {{ producto.nombre }} - Precio: ${{ producto.precio }}
+          <button
+            @click="eliminarProducto(producto)"
+            class="btn btn-danger btn-sm"
+          >
+            Eliminar
+          </button>
+        </li>
+      </ul>
+      <button @click="vaciarCarrito" class="btn btn-danger">
+        Vaciar carrito
+      </button>
+      <button
+        @click="realizarCompra"
+        :disabled="carrito.length === 0"
+        class="btn btn-success"
+      >
+        Realizar compra
+      </button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        productos: [
-          { nombre: "Jabón verde", descripcion: "No contamina", precio: 10 },
-          { nombre: "Shampoo suave", descripcion: "Para todo tipo de cabello", precio: 15 },
-          { nombre: "Crema hidratante", descripcion: "Fórmula enriquecida con vitaminas", precio: 20 }
-        ],
-        carrito: []
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      productos: [
+        { nombre: "Jabón verde", descripcion: "No contamina", precio: 10 },
+        {
+          nombre: "Shampoo suave",
+          descripcion: "Para todo tipo de cabello",
+          precio: 15,
+        },
+        {
+          nombre: "Crema hidratante",
+          descripcion: "Fórmula enriquecida con vitaminas",
+          precio: 20,
+        },
+      ],
+      carrito: [],
+    };
+  },
+  methods: {
+    calcularTotal() {
+      let total = 0;
+      for (let i = 0; i < this.carrito.length; i++) {
+        total += this.carrito[i].precio;
+      }
+      return total;
+    },
+    agregarProducto(producto) {
+      this.carrito.push(producto);
+    },
+    eliminarProducto(producto) {
+      const index = this.carrito.indexOf(producto);
+      if (index > -1) {
+        this.carrito.splice(index, 1);
       }
     },
-    methods: {
-      calcularTotal() {
-        let total = 0;
-        for (let i = 0; i < this.carrito.length; i++) {
-          total += this.carrito[i].precio
-        }
-        return total;
-      },
-      agregarProducto(producto) {
-        this.carrito.push(producto)
-      },
-      vaciarCarrito() {
-        this.carrito = []
-      },
-      realizarCompra() {
-        alert("Compra realizada. Gracias por su compra.");
-        alert("Vuelva prontos...");
-        this.vaciarCarrito();
-      }
-    }
-  }
-  </script>
-  
-  <style>
-  
-  </style>
-  
+    vaciarCarrito() {
+      this.carrito = [];
+    },
+    realizarCompra() {
+      alert("Compra realizada. Gracias por su compra.");
+      alert("Vuelva prontos...");
+      this.vaciarCarrito();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.alert {
+  margin-top: 1rem;
+}
+
+.list-group {
+  margin-top: 1rem;
+}
+
+.btn {
+  margin-top: 1rem;
+}
+</style>
