5f07e2254d06a58b8b59c77a11dc06de44cbf848
SergioCamejo <sergiofcamejo@yahoo.com.ar>
2023-06-14 22:20:20 -0300

    cambios 22:20 h

---
+++ frontend/src/views/Carrito.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>Carrito de compras</h1>
+      <Carrito />
+    </div>
+  </template>
+  
+  <script>
+  import Carrito from "@/components/Carrito.vue"
+  
+  export default {
+    components: {
+      Carrito
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
