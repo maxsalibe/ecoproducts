f1429df3bbbb73ba63bf6f8b79b0d9caa7613bf1
MicaelaJalife <micajalife@gmail.com>
2023-06-15 23:27:09 -0300

    base de datos mongo ATLAS, productos + usuarios cargados en base

--- back end/config.js
+++ back end/config.js
@@ -1,7 +1,8 @@
 const PORT = 8080
 const MODO_PERSISTENCIA = 'MONGODB'   
-const STRCNX = 'mongodb://127.0.0.1'
-const BASE = 'mibase'
+//const STRCNX = 'mongodb://127.0.0.1'
+const STRCNX = 'mongodb+srv://mica:tp2sanchez@cluster0.h9xwixn.mongodb.net/?retryWrites=true&w=majority'
+const BASE = 'EcoCommerce'
 
 export default {
     PORT,   
--- back end/servicio/productos.js
+++ back end/servicio/productos.js
@@ -13,15 +13,15 @@ class Servicio {
         return productos
     }
 
-/*     guardarProducto = async producto => {
-        const res = validar(producto)
+     guardarProducto = async producto => {
+        //const res = validar(producto)
         if(res.result) {
             const productoGuardado = await this.model.guardarProducto(producto)
             return productoGuardado
         } else {
             throw res.error
         }
-    } */
+    } 
 
     actualizarProducto = async (id, producto) => {
         const productoActualizado = await this.model.actualizarProducto(id, producto)
